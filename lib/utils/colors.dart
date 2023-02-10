c97ec35173ae9fe903e141530066857e6509a9b5
pnaskardev <pnaskardev@gmail.com>
2023-02-10 20:22:44 +0530

    added colors themes and styles

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
+const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const defaultColor = Colors.white;
+const primaryColor = Color(0xff619549);
+const secondaryColor = Colors.grey;
+const selectedColor = Colors.green;
+const homepageborderColor = Color.fromARGB(41, 244, 244, 244);
+const profileCardBGColor = Color.fromARGB(225, 234, 234, 234);
+const redColor = Color.fromARGB(255, 240, 12, 12);
+const serviceCardBorderColor = Color(0xffD9D9D9);
