75cdeb3f743ee186b48a39cdc8128c4a9d29ff12
pnaskardev <pnaskardev@gmail.com>
2023-02-17 23:37:16 +0530

    added required models

---
+++ lib/models/medicine.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+
+class Medicine
+{
+  final String name;
+  final String description;
+  final String type;
+  final int freq;
+  final List<DateTime> timeList;
+  final int days;
+
+  Medicine
+  ({
+    required this.name,
+    required this.description,
+    required this.type,
+    required this.freq,
+    required this.timeList,
+    required this.days
+  });
+
+  
+
+
+}
---
+++ lib/models/user_data.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:medi_rem/models/medicine.dart';
+
+class UserData
+{
+  String? uid;
+  String? email;
+  List<Medicine>? medicineList;
+  UserData({this.uid,this.email});
+}
